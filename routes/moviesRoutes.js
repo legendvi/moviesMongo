16f36d120dd817b299de1b25de56cd4fda80da6a
legendvi <dvigneshkumar3@gmail.com>
2022-01-08 13:32:52 +0530

    updated

--- routes/moviesRoutes.js
+++ routes/moviesRoutes.js
@@ -9,43 +9,45 @@ import {
   deleteMovieById,
 } from "../helper.js";
 const routes = express.Router();
-routes.get("", async (req, res) => {
-  const filters = req.query;
-  if (filters.rating) filters.rating = +filters.rating;
-  const filteredUser = await getMovies(filters);
-  res.json(filteredUser);
-});
+routes
+  .route("/")
+  .get(async (req, res) => {
+    const filters = req.query;
+    if (filters.rating) filters.rating = +filters.rating;
+    const filteredUser = await getMovies(filters);
+    res.json(filteredUser);
+  })
+  .post(async (req, res) => {
+    const movies = req.body;
+    console.log(movies);
+    const result = await createMovies(movies);
 
-routes.get("/:id", async (req, res) => {
-  const id = req.params.id;
-  const result = await getMoviesById(id);
-  res.send(result ? result : { msg: "Movie not Found" });
-});
-routes.post(async (req, res) => {
-  const movies = req.body;
-  console.log(movies);
-  const result = await createMovies(movies);
+    res.send(result ? result : { msg: "Movie not Found" });
+  });
 
-  res.send(result ? result : { msg: "Movie not Found" });
-});
+routes
+  .route("/:id")
+  .get(async (req, res) => {
+    const id = req.params.id;
+    const result = await getMoviesById(id);
+    res.send(result ? result : { msg: "Movie not Found" });
+  })
+  .put(async (req, res) => {
+    const id = req.params.id;
+    const updatedMovie = req.body;
+    console.log(movies);
+    const result = await updateMovieByID(id, updatedMovie);
 
-routes.put("/:id", async (req, res) => {
-  const id = req.params.id;
-  const updatedMovie = req.body;
-  console.log(movies);
-  const result = await updateMovieByID(id, updatedMovie);
+    res.send(result ? result : { msg: "Movie not Found" });
+  })
+  .delete(async (req, res) => {
+    const id = req.params.id;
+    const movies = req.body;
 
-  res.send(result ? result : { msg: "Movie not Found" });
-});
+    console.log(movies);
+    const result = await deleteMovieById(id);
 
-routes.delete("/:id", async (req, res) => {
-  const id = req.params.id;
-  const movies = req.body;
-
-  console.log(movies);
-  const result = await deleteMovieById(id);
-
-  res.send(result ? result : { msg: "Movie not Found" });
-});
+    res.send(result ? result : { msg: "Movie not Found" });
+  });
 
 export default routes;
